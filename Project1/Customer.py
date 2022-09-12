1974f92f4f0c4b4cd6462de532ed522b1448ca3b
marco-tonnarelli <marco.tonnarelli@mail.polimi.it>
2022-09-12 16:37:03 +0200

    Pulizia

--- Project1/Customer.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Customer:
-    def __init__(self, demand_curves, n_users, alpha_ratios, n_products, graph_probabilities):
-        self.alphas_ratios = alpha_ratios
-        self.n_users = n_users
-        self.n_products = n_products
-        self.demand_curves = demand_curves
-        self.graph_probabilities = graph_probabilities
