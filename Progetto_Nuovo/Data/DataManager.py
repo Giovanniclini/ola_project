7c58b7dc9821215cfab16385a5efe779c81d6e20
Giovanni Clini <gioviclini@gmail.com>
2022-10-06 16:43:09 +0200

    aggiunta funzione per creazione classe aggregata utenti

--- Progetto_Nuovo/Data/DataManager.py
+++ Progetto_Nuovo/Data/DataManager.py
@@ -31,7 +31,33 @@ def evaluate_aggregate_conversion_rates(customer_classes):
 def evaluate_aggregate_alphas(customer_classes):
     return None
 
+def get_customer_class_from_json_aggregate(file_name_class_1, file_name_class_2, file_name_class_3):
+    # Load file customer class non aggregate
+    file_class_1 = open(file_name_class_1)
+    file_class_2 = open(file_name_class_2)
+    file_class_3 = open(file_name_class_3)
 
+    # Extract data from JSON for user class
+    data_class_1 = json.load(file_class_1)
+    data_class_2 = json.load(file_class_2)
+    data_class_3 = json.load(file_class_3)
+
+    # Assign aggregate values tu agg customer class
+    customer_class_aggregate = CustomerClass(0)
+    # Agg number of customers
+    customer_class_aggregate.number_of_customers = data_class_1['n_users'] + data_class_2['n_users'] + data_class_3['n_users']
+    # Agg average alphas
+    temp_matrix = data_class_1['n_users'] * data_class_1['average_alphas'] + data_class_2['n_users'] * data_class_2['average_alphas'] + data_class_3['n_users'] * data_class_3['average_alphas']
+    customer_class_aggregate.alpha_probabilities = temp_matrix / customer_class_aggregate.number_of_customers
+    # Agg reservation prices
+    temp_matrix = data_class_1['n_users'] * data_class_1['reservation_prices'] + data_class_2['n_users'] * data_class_2['reservation_prices'] + data_class_3['n_users'] * data_class_3['reservation_prices']
+    customer_class_aggregate.alpha_probabilities = temp_matrix / customer_class_aggregate.number_of_customers
+    # Agg graph probabilities
+    temp_matrix = data_class_1['n_users'] * data_class_1['graph_probabilities'] + data_class_2['n_users'] * data_class_2['graph_probabilities'] + data_class_3['n_users'] * data_class_3['grapg_probabilities']
+    customer_class_aggregate.alpha_probabilities = temp_matrix / customer_class_aggregate.number_of_customers
+
+    return customer_class_aggregate
+    
 def evaluate_aggregate_graph_probabilities(customer_classes):
     return None
 
