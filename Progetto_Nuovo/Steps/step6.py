218d460ad7f63d74e2abb7a4a4c8fedbaec310aa
alicebrugnoli <79971625+alicebrugnoli@users.noreply.github.com>
2022-10-14 18:01:46 +0200

    step 6 - SW

--- Progetto_Nuovo/Steps/step6.py
+++ Progetto_Nuovo/Steps/step6.py
@@ -1,6 +1,5 @@
 from termcolor import colored
 from Progetto_Nuovo.Environment.Environment import *
-from Progetto_Nuovo.Learners.TSLearner import *
 from Progetto_Nuovo.Learners.SWUCBLearner import *
 from Progetto_Nuovo.generateData import *
 from Progetto_Nuovo.Data.DataManager import *
@@ -19,7 +18,7 @@ user_class_filename = "../Data/user_class_aggregate.json"
 max_units_sold = 2
 
 if __name__ == '__main__':
-    print(colored('\n\n---------------------------- STEP 3 ----------------------------', 'blue', attrs=['bold']))
+    print(colored('\n\n---------------------------- STEP 6 ----------------------------', 'blue', attrs=['bold']))
 
     # assign graph from json file
     graph = get_graph_from_json(graph_filename)
@@ -29,9 +28,6 @@ if __name__ == '__main__':
     configurations = initialization_other_steps(prices)
     # generate the customer class from json (aggregate)
     customer_class = get_customer_class_from_json(user_class_filename)
-
-    # init reward collection for each experiment TS
-    rewards_per_experiment_ts = []
     # init reward collection for each experiment UCB
     rewards_per_experiment_ucb = []
 
@@ -42,30 +38,20 @@ if __name__ == '__main__':
         print("Experiment {0}...".format(e))
         # init environment
         env = Environment(n_prices, customer_class, lambda_coefficient, n_products)
-        # init Thompson Sampling learner
-        ts_learner = TSLearner(n_prices, n_products)
         # init UCB-1
         ucb_learner = SWUCBLearner(n_prices, n_products)
         total_seen_ucb = np.zeros((n_products, n_prices))
         total_seen_product_ucb = np.zeros(n_products)
+        total_seen_product_per_window_ucb = np.zeros(n_products)
         # for each day
         for t in tqdm(range (0, number_of_days)):
             alpha_ratios = np.random.dirichlet(customer_class.alpha_probabilities)
             item_sold_mean = customer_class.item_sold_mean
 
             # initialize for the next window of 25 days
-            if number_of_days % 25 == 0:
+            if number_of_days % window == 0:
                 total_seen_ucb = np.zeros((n_products, n_prices))
-                total_seen_product_ucb = np.zeros(n_products)
-
-            # THOMPSON SAMPLING
-            # pull prices belonging to a configuration (super arm)
-            pulled_config_indexes_ts = ts_learner.pull_arm()
-            # collect reward trough e-commerce simulation for all the users
-            reward_ts, units_sold_ts, total_seen_ts = env.round(pulled_config_indexes_ts, prices, alpha_ratios,
-                                                                item_sold_mean)
-            # update TS learner parameters
-            ts_learner.update(pulled_config_indexes_ts, units_sold_ts, np.sum(total_seen_ts[1:]), reward_ts)
+                total_seen_product_per_window_ucb = np.zeros(n_products)
 
             # UCB-1
             pulled_config_indexes_ucb = ucb_learner.pull_arm()
@@ -78,20 +64,14 @@ if __name__ == '__main__':
             for product in range(len(pulled_config_indexes_ucb)):
                 total_seen_ucb[product, pulled_config_indexes_ucb[product]] += np.sum(total_seen_daily_ucb[1:])
                 total_seen_product_ucb[product] += np.sum(total_seen_daily_ucb[1:])
+                total_seen_product_per_window_ucb[product] += np.sum(total_seen_daily_ucb[1:])
             ucb_learner.update(pulled_config_indexes_ucb, units_sold_ucb, total_seen_since_daybefore_ucb, total_seen_ucb,
-                               total_seen_product_ucb, reward_ucb)
+                               total_seen_product_ucb, total_seen_product_per_window_ucb, reward_ucb)
 
-        # append collected reward of current experiment TS
-        rewards_per_experiment_ts.append(ts_learner.collected_rewards)
         # append collected reward of current experiment UCB
         rewards_per_experiment_ucb.append(ucb_learner.collected_rewards)
-        # printTSBeta(learner.beta_parameters[:, 0, :], rewards_per_experiment[0])
-        # print("SOS", learner.collected_rewards[0])
-    printReward(rewards_per_experiment_ts, clairvoyant)
-    printReward(rewards_per_experiment_ucb, clairvoyant)
 
-    evaluate_mean_std_rewards(rewards_per_experiment_ts)
-    printRegret(rewards_per_experiment_ts, clairvoyant)
+    printReward(rewards_per_experiment_ucb, clairvoyant)
     printRegret(rewards_per_experiment_ucb, clairvoyant)
 
 
