1298dd94d32c9cdbbe9b08f49322447dbbe87f12
Ma7i5ab3 <92745042+Ma7i5ab3@users.noreply.github.com>
2022-10-31 10:46:56 +0100

    Social Influence

--- Progetto_Nuovo/Data_Structures/CustomerClass.py
+++ Progetto_Nuovo/Data_Structures/CustomerClass.py
@@ -14,15 +14,13 @@ class CustomerClass:
         self.item_sold_mean = np.zeros((5, 4))
         # assign a random value to the reservation price for each product
         self.reservation_prices = [np.random.uniform(50., 301.) for _ in range(5)]
+        # keep track of the products clicked
+        self.units_clicked_for_each_product = np.zeros(5)
         # keep track of number of units sold of each product (global)
         self.units_purchased_for_each_product = np.zeros(5)
         self.units_purchased_per_product_per_campaign = np.random.randint(0, 20, size=(17, 5))
         # assign graph table empty, it seems that must replace the social_influence_transition_probability_matrix
         self.graph_probabilities = np.zeros((5, 5))
-        # assign a random value to the social influence transition probability matrix to navigate among the products
-        self.social_influence_transition_probability_matrix = np.random.uniform(0, 1, (5, 5))
-        # put zero the diagonal because a product already seen cannot be seen anymore
-        np.fill_diagonal(self.social_influence_transition_probability_matrix, 0)
         # assign empty global history
         self.global_history = []
 
